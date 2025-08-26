9f7661ff1d4ec1f2c5fda24e038042e4b36db966
Regiane Melo <regianemelo1995@gmail.com>
2025-08-26 15:10:17 -0300

    estilizando primeira secao

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import { Button } from "flowbite-react";
+
+export function Hero () {
+  return ( 
+    <section className="bg-white dark:bg-gray-900">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Curso Prático da Lei Maria da Penha</h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Domine todas as técnicas necessárias para advogar na área da violência doméstica, tanto como defensor como assistente de acusação.</p>
+          <div className="flex flex-wrap gap-2">
+            <Button size="xl" className="bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg">
+                QUERO ADQUIRIR MEU CURSO
+              </a>
+            </Button>
+          </div>
+        </div>
+        <div className="lg:mt-0 lg:col-span-5 lg:flex">
+          <img src="../assets/img-hero.jpg"  alt="Logo" />
+        </div>                
+    </div>
+</section> 
+)
+}
