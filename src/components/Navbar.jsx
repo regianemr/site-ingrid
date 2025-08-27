3560abd0161a71a5db5d8718433e8227a85de316
Regiane Melo <regianemelo1995@gmail.com>
2025-08-27 13:03:58 -0300

    mudancas de cores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export function MyNavbar() {
     <ThemeProvider theme={navTheme}>
       <Navbar fluid rounded>
         <NavbarBrand href="/">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Logo" />
+          {/* <img src="/" className="mr-3 h-6 sm:h-9" alt="Logo" /> */}
           <span className="bg-self-center whitespace-nowrap text-xl font-semibold dark:text-white">Ingrid Arcanjo</span>
         </NavbarBrand>
         <NavbarToggle />
@@ -15,11 +15,11 @@ export function MyNavbar() {
             Início
           </NavbarLink>
           <NavbarLink href="#">
-            Quem Sou
+            Sobre o Curso
           </NavbarLink>
-          <NavbarLink href="#">Áreas de Atuação</NavbarLink>
+          <NavbarLink href="#">Diferenciais</NavbarLink>
+          <NavbarLink href="#">Quem sou</NavbarLink>
           <NavbarLink href="#">Perguntas</NavbarLink>
-          <NavbarLink href="#">Contato</NavbarLink>
         </NavbarCollapse>
       </Navbar>
     </ThemeProvider>
