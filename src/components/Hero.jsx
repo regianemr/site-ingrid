a51df2099e0381f8d04d60bf3fedd32e6e70cd70
Regiane Melo <regianemelo1995@gmail.com>
2025-08-27 13:03:09 -0300

    mudanca no botao

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,9 @@ export function Hero () {
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Curso Prático da Lei Maria da Penha</h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Domine todas as técnicas necessárias para advogar na área da violência doméstica, tanto como defensor como assistente de acusação.</p>
           <div className="flex flex-wrap gap-2">
-            <Button size="xl" className="bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800">
-              <a href="#" className="inline-flex items-center justify-center px-8 py-3 mr-3 text-base font-medium text-center text-white rounded-lg">
-                QUERO ADQUIRIR MEU CURSO
+            <Button size="xl" className="md:justify-items-center bg-gradient-to-r from-purple-500 to-pink-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800 rounded-full">
+              <a href="#" className="inline-flex items-center justify-center px-8 py-3 mr-3 text-xs md:text-base font-medium text-center text-white rounded-lg">
+                QUERO ADQUIRIR O CURSO
               </a>
             </Button>
           </div>
